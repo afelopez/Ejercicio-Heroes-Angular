d9a7c5edd608ac6a3925a34210931d93807a53a6
Andrés López A <ing.ratosocial@gmail.com>
2018-10-09 00:15:11 -0500

    displaying lists

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from "../hero"; // importo la clase Hero del archivo hero.ts que está en la raiz /app
+import { Hero } from "../hero"; // importo la clase Hero del archivo hero.ts que está en la raiz /app
+import { HEROES } from '../mock-heroes'
 
 @Component({
   selector: 'app-heroes', // componente del CSS selector (es el elemento que identifica la vista en el HTML)
@@ -8,16 +9,15 @@ import {Hero} from "../hero"; // importo la clase Hero del archivo hero.ts que e
 })
 export class HeroesComponent implements OnInit {
   // agrego variable la puedo llamar en el html del componente
-  hero_last = 'Windstorm';
-  // crea la estancia de la clase Hero
-  hero: Hero = {
-    id: 1,
-    name: "Windstorm"
-  };
+  selectedHero: Hero;
+  // crea la estancia de la clase Hero para el FOR
+  heroes = HEROES;
   // constructor
   constructor() { }
   // ayudante de inicio
   ngOnInit() {
   }
-
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+// Usa la clase hero por supuesto
+import { Hero } from './hero';
+// Genera una constante "inmodificable" del objeto Hero
+export const HEROES: Hero[] = [
+  { id: 0, name: 'Mr. Raki' },
+  { id: 1, name: 'Mr. Nice' },
+  { id: 2, name: 'Narco' },
+  { id: 3, name: 'Bombasto' },
+  { id: 4, name: 'Celeritas' },
+  { id: 5, name: 'Magneta' },
+  { id: 6, name: 'RubberMan' },
+  { id: 7, name: 'Dynama' },
+  { id: 8, name: 'Dr IQ' },
+  { id: 9, name: 'Magma' },
+  { id: 0, name: 'Tornado' }
+];
