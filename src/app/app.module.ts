be9fde0a88bf71120ba82bbdaa82f46849bcced4
Andrés López A <ing.ratosocial@gmail.com>
2018-10-08 23:42:09 -0500

    adding components and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
+    // aqui se declaran los componentes despues de importados
     AppComponent,
     HeroesComponent
   ],
   imports: [
-    BrowserModule
+    // aqui se delcaran los modulos de ng despues de importarlos
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
