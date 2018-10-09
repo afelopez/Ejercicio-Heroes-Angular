e4b1fefd8e3080e13922dcc10c4e04575aca489e
Andrés López A <ing.ratosocial@gmail.com>
2018-10-09 00:39:21 -0500

    separate master y detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     // aqui se declaran los componentes despues de importados
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     // aqui se delcaran los modulos de ng despues de importarlos
